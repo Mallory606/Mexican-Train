9666333c39116fb0e6c92bf110280767a3f77014
Ashley K <buginla@gmail.com>
2020-09-10 22:57:34 -0600

    Added early implementation for setting up a game starting with a test to make sure I am reading input correctly. Test was successful

--- src/MexicanTrainManager.java
+++ src/MexicanTrainManager.java
@@ -1,12 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MexicanTrainManager{
     private List<Domino> boneyard;
     private List<Player> players;
+    private List<Domino> mexicanTrain;
+    private List<List<Domino>> playerTrains;
+    private boolean consoleGame;
+    private int numPlayers;
 
-    public MexicanTrainManager(){
+    public MexicanTrainManager(boolean console){
+        consoleGame = console;
         initializeBoneyard();
+        if(consoleGame){
+            try{ consoleGame(); }
+            catch(IOException e){ e.printStackTrace(); }
+        }
     }
 
     private void initializeBoneyard(){
@@ -21,5 +33,17 @@ public class MexicanTrainManager{
     private Domino pullFromBoneyard(){
         int rand = (int)(Math.random()*boneyard.size());
         return boneyard.remove(rand);
+    }
+
+    private void consoleGame() throws IOException{
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        int numberInput;
+        System.out.println("Let's play Mexican Train!");
+        System.out.println("How many players?");
+        input = in.readLine();
+        numberInput = Integer.parseInt(input);
+        numPlayers = numberInput;
+        System.out.println(numberInput + " players!");
     }
 }
--- src/MexicanTrainMaster.java
+++ src/MexicanTrainMaster.java
@@ -1,7 +1,8 @@
 public class MexicanTrainMaster{
     public static void main(String[] args){
+        MexicanTrainManager manager;
         if(args[0].equals("console")){
-
+            manager = new MexicanTrainManager(true);
         }
         else{
 
